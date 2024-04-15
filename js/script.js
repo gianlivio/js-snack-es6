b30f25b3165443669d13a1e5b304495333d0af09
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-04-15 16:26:13 +0200

    add: snack 1

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const bici = [
+    { nome: "bici1",
+      peso: 5
+    },
+    { nome: "bici2",
+      peso: 6
+    },
+    { nome: "bici3",
+      peso: 8
+    },
+    { nome: "bici4",
+      peso: 13
+    },
+    { nome: "bici5",
+      peso: 4
+    },
+];
+
+
+// ciclo forEach per trovare bici più leggera
+
+let biciLeggera = bici[0];
+
+bici.forEach(bicicletta => {
+if (bicicletta.peso < biciLeggera.peso) {
+    biciLeggera = bicicletta;
+    console.log(`${bicicletta.nome}, ${bicicletta.peso}`)
+}
+
+})
